646ecd5ffa4befb993e0fff60fe0ba22e9c9cf59
Diego Fernandes <diegoti19833@gmail.com>
2022-11-16 17:13:58 -0300

    Update TaskList.tsx

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,43 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+   
+     if(!newTaskTitle) return
+
+     const newTitle = {
+       id: Math.random(),
+       title: newTaskTitle,
+       isComplete: false
+     }
+
+     setTasks(ol => [...tasks, newTitle])
+      
   }
+
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const newTitle = tasks.map(task => task.id === id ?{
+     ...task,
+     isComplete: !task.isComplete
+    }
+    : task
+
+   
+     
+  )
+    setTasks(newTitle)
   }
+
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+   
+     const taskFilter = tasks.filter(task => task.id !== id)
+
+     setTasks(taskFilter)
+    
   }
 
   return (
